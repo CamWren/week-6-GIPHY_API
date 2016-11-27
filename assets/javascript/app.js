142f4a444c772215034f11e479773cf76b011f87
CamWren <wrnecmrn@gmail.com>
2016-11-27 11:40:11 -0600

    Initial version with GIPHY API public beta key

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,55 +20,21 @@ $(document).ready(function() {
 
 
 //Figure out how to insert strings in to ajax query
-//May need to replace queryButtons class with id and assign value to text of giving string
+//May need to replace queryButtons class with id and assign value to text of given string
 	$(".queryButton").click(function() {
-		
-		var query = queries[i].split(' ').join('+');
-		console.log(query);
-		/*var queryURL = "http://api.giphy.com/v1/gifs/search?q=""&api_key=dc6zaTOxFJmzC";	
-		$.ajax({url:queryURL,method:'GET'})
-			.done(function(response) {
-				console.log(response);
-			});*/
-	
+		var individualQuery = queries[i];
+		for (key in queries) {
+        	var query = queries[key].split(' ').join('+');
+        	console.log(query);
+        /*var queryURL = "http://api.giphy.com/v1/gifs/search?q=""&api_key=dc6zaTOxFJmzC";    
+        $.ajax({url:queryURL,method:'GET'})
+            .done(function(response) {
+                console.log(response);
+            });*/
+        };
 	});
-
 
 });
 
 
 
-//First Attempt to create buttons from an array - doesn't work
-/*function topicButton () {
-		for (var i = 0; i < topics.length; i++) {
-			var topicButton = document.createElement("button");
-			topicButton.setAttribute("id", "topicButton");
-			var newTopic = document.createTextNode(topics[i]);
-			topicButton.appendChild(newTopic);
-			console.log(topics[i]);
-		};
-	};*/
-
-//Second attempt to create buttons from an array - works
-/*function topicButton () {
-		for (var i = 0; i < topics.length; i++) {
-			var topicButton = document.createElement("button");
-			topicButton.setAttribute("id", "topicButton");
-			var newTopic = document.createTextNode(topics[i]);
-			topicButton.appendChild(newTopic);
-			$("#container").html(topicButton);
-			console.log(topics[i]);
-		};
-	};*/
-
-//Third attempt to create buttons from an array - works
-/*function topicButton () {
-		for (i = 0; i < topics.length; i++) {
-			var topicButton = document.createElement("button");
-			topicButton.setAttribute("id", "topicButton");
-			var newTopic = document.createTextNode(topics[i]);
-			topicButton.appendChild(newTopic);
-			$("#topicButtons").html(topicButton);
-			console.log(topics[i]);
-		};
-	};*/
